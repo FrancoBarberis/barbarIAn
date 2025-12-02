b33656e47a35a824b23aa2fcd61ede6fbfe87b1e
Franco Barberis <francobarberissic3@gmail.com>
2025-12-02 11:46:18 -0300

    Main layout ordenado

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,23 +1,33 @@
-
-import { layout, header, body, sidebar, content, footer } from './MainLayout.module.css';
+import {
+  layout,
+  header,
+  body,
+  sidebar,
+  content,
+  footer,
+} from "./MainLayout.module.css";
 
 type MainLayoutProps = {
-    children: React.ReactNode;
-    showSidebar?: boolean;
-    title?: string;
+  children: React.ReactNode;
+  showSidebar?: boolean;
+  title?: string;
 };
 
-export const MainLayout: React.FC<MainLayoutProps> = ({ children, showSidebar = true, title = "BarbarIAn" }) => {
-    return (
-        <div className={layout}>
-            <header className={header}>
-                <h1>{title}</h1>
-            </header>
-            <div className={body}>
-                {showSidebar && <aside className={sidebar}>Sidebar</aside>}
-                <main className={content}>{children}</main>
-            </div>
-            <footer className={footer}>Footer</footer>
-        </div>
-    );
+export const MainLayout: React.FC<MainLayoutProps> = ({
+  children,
+  showSidebar = true,
+  title = "BarbarIAn",
+}) => {
+  return (
+    <div className={layout}>
+      {showSidebar && <aside className={sidebar}>Sidebar</aside>}
+      <div className={body}>
+        <header className={header}>
+          <h1>{title}</h1>
+        </header>
+        <main className={content}>{children}</main>
+        <footer className={footer}>Footer</footer>
+      </div>
+    </div>
+  );
 };
