5a57ffccabe06863ae92b1a776704e51de6b2a8d
Franco Barberis <francobarberissic3@gmail.com>
2025-12-04 12:18:40 -0300

    Clsx Collapsed

--- src/components/ChatSidebar/ChatSidebar.tsx
+++ src/components/ChatSidebar/ChatSidebar.tsx
@@ -1,11 +1,25 @@
 import styles from "./ChatSidebar.module.css";
 import UserProfile from "../UserProfile/UserProfile";
+import { useState } from "react";
+import clsx from "clsx";
 
 const ChatSidebar: React.FC = () => {
+  const [collapsed,setCollapsed] = useState(false);
   return (
-    <div className={styles.chatSidebar}>
-      <button className={styles.toggleSidebar}>X</button>
-      <h2>Chat Sidebar</h2>
+    <div className={clsx(
+      styles.chatSidebar,
+      {[styles.collapsed]:collapsed}  
+    )}
+      >
+      <button className={styles.toggleSidebar} onClick={()=>setCollapsed(prev=>!prev)}>{collapsed ? "Abrir" : "X"}</button>
+      <ul className={styles.messageList}>
+        <li className={styles.message}>Mensaje1sadasdasdasdsadsadasdasdasd</li>
+        <li>Mensaje2dsadasdasdsadasdsdsadsadsadsadsds</li>
+        <li>Mensaje1dsadsadsadsadsadsadsadsadsadsad</li>
+        <li>Mensaje3sadsadasdasdasdsadasdasdasdas</li>
+        <li>Mensaje4</li>
+        <li>Mensaje5</li>
+      </ul>
       <UserProfile />
     </div>
   );
