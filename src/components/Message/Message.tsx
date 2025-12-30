56e53e06e6705ea25dbbdf7e6c81a684f3293e15
Franco Barberis <francobarberissic3@gmail.com>
2025-12-30 00:35:06 -0300

    MUI Message

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,5 +1,6 @@
 import styles from "./Message.module.css";
 import clsx from "clsx";
+import Card from "@mui/joy/Card";
 
 interface MessageProps {
   text: string;
@@ -8,27 +9,33 @@ interface MessageProps {
 }
 
 const Message: React.FC<MessageProps> = ({ text, role, timestamp }) => {
-
-  
   const formattedTime = new Date(timestamp).toLocaleTimeString("es-AR", {
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
   });
-  
+
   return (
-    <div
-      className={clsx(
-        styles.message, // estilos comunes (burbuja, padding, etc.)
-        role === "assistant" && styles.assistant,
-        role === "user" && styles.user
-      )}
+    <Card
+      orientation="horizontal"
+      size="lg"
+      variant="soft"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minWidth: "20%",
+        backgroundColor: role === "assistant" ? "blue" : "green",
+        color: "black",
+        alignSelf: role === "assistant" ? "flex-end" : "flex-start",
+        textAlign: role === "assistant" ? "right" : "left",
+        marginRight: role === "assistant" ? "2%" : "0",
+        marginLeft: role === "user" ? "2%" : "0",
+      }}
+      className={clsx(styles.message)}
     >
-      <h3 className={styles.message_text}>
-        {text}
-      </h3>
+      <p className={styles.message_text}>{text}</p>
       <p className={styles.timestamp}>{formattedTime}</p>
-    </div>
+    </Card>
   );
 };
 
